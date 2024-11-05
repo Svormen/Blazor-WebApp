5eb7416f1b0806f30fa31ca5e5e61a521f50bb74
Svormen <62176082+Svormen@users.noreply.github.com>
2024-11-05 16:50:47 +0100

    adding effect for projects boxes

--- BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d194beccae2e5b913bdb0176fdc0d707b9aaed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a592aa3968794485095a6dab7e069f84cbfdda")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
