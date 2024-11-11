e11175478a5cb9a2d5172ed58a7a716794ce71c4
Svormen <62176082+Svormen@users.noreply.github.com>
2024-11-11 17:20:06 +0100

    adding new project width

--- BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a89a97670075a67b6539e596c3245ff6ef5673c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c23ebb2198ebb5aac59cd7eafe4d72324796589")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
