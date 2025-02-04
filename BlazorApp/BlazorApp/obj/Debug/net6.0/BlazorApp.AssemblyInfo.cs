0a563344bbaeaec01c0a4f8d1b15789546158aa5
Svormen <62176082+Svormen@users.noreply.github.com>
2025-02-04 01:04:44 +0100

    adding counter into counter page

--- BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c434692368775e794663f4dfcc4e9d8e76db75fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe9b920f8260ee8f519ed3981d6efe61db3c994e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
