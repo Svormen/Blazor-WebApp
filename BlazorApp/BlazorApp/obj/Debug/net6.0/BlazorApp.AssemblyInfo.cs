cb1597e99916aad43b3a09d22406458bd8aa4495
Svormen <62176082+Svormen@users.noreply.github.com>
2025-02-13 12:18:33 +0100

    new weather structure

--- BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57f934ba7c9aabf6254f7fa1e0a4e6b4be5a968c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4ec850311ac3881072b232dcbc015e20b5e689c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
