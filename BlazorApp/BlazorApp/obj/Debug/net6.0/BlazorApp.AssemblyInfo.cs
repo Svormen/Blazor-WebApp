9688b9960061f04d53d3b7101932395fce83f6d0
Svormen <62176082+Svormen@users.noreply.github.com>
2025-02-16 19:04:38 +0100

    changed font size of cloud and temperature data

--- BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/BlazorApp/obj/Debug/net6.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a38c1b2e58a229d27540047dcd4abaa726d7d7da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a473230aec798104b137431d6d27ee5a399b5dca")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
