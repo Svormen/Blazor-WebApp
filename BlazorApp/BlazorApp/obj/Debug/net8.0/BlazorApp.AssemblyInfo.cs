913d07a5a53b3ec94920ca03d30e3b8c646f0ad3
Svormen <62176082+Svormen@users.noreply.github.com>
2025-02-22 00:17:02 +0100

    adding style for button

--- BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bdf55da2f0f8d9e947d71c237b95236adc2e457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3adab7460558eafc00679a0ab67a42ae313ed030")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
