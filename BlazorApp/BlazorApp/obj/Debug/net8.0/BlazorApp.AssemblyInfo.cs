78e6edb73e4b36f0b9f94b0200be7710a24d5a7d
Svormen <62176082+Svormen@users.noreply.github.com>
2025-02-26 11:10:11 +0100

    changing font size of weather app

--- BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913d07a5a53b3ec94920ca03d30e3b8c646f0ad3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0cf1c01a611a0b7370b06184f757149d587bc32")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
