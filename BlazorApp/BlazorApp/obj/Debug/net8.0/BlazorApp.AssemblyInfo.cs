0d32394c951143e83a97907a0b9b2d8897c5804e
Svormen <62176082+Svormen@users.noreply.github.com>
2025-03-05 17:41:10 +0100

    change design of buttons

--- BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9939ed727130cf83766582d98357157865b94cb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023b7bd17eccc6fd72e238f189647881d58cb5a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
