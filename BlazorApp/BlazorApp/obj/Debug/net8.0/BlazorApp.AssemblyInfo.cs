9aafe12032731bf28bd784929c9f91001b6295d6
Svormen <62176082+Svormen@users.noreply.github.com>
2025-03-08 09:29:54 +0100

    adding hint functionality to game

--- BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46357f84603404b5e1efe7332153cac010e6eaa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de5fbb51e95f06fcbb072a2a0eb4ab83afee387f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
