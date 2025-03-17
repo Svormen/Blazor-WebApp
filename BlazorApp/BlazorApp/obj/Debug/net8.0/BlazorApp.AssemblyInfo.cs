8f84fa13ca757a066b3f3feebdd71aae3b4fa0db
Svormen <62176082+Svormen@users.noreply.github.com>
2025-03-17 23:57:06 +0100

    changing .net version

--- BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aafe12032731bf28bd784929c9f91001b6295d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adc7418b4fc60b5aa10b6ed6616b4aa59dc0ff79")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
